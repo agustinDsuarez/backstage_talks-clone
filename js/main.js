1235aaa0abd7106708a3d83791e6cfa63dedc7cb
agustín <daniel.babarzi@gmail.com>
2023-06-15 17:39:55 -0300

    detalles

--- js/main.js
+++ js/main.js
@@ -7,19 +7,19 @@ window.addEventListener("scroll", () => {
   if (scroll < 300) {
     // issue #7
     body.style.backgroundColor = "#ff608c";
-  } else if (scroll >= 300 && scroll < 900) {
+  } else if (scroll >= 300 && scroll < 1200) {
     // issue #6
     body.style.backgroundColor = "#ffffff";
   } else if (scroll >= 1200 && scroll < 1800) {
     // issue #5
     body.style.backgroundColor = "#00c1b5";
-  } else if (scroll >= 2000 && scroll < 2600) {
+  } else if (scroll >= 1800 && scroll < 2600) {
     // issue #4
     body.style.backgroundColor = "#ff651a";
-  } else if (scroll >= 2800 && scroll < 3400) {
+  } else if (scroll >= 2600 && scroll < 3400) {
     // issue #3
     body.style.backgroundColor = "#ffbe00";
-  } else if (scroll >= 3600 && scroll < 4200) {
+  } else if (scroll >= 3400 && scroll < 4200) {
     // issue #2
     body.style.backgroundColor = "#1d3fbb";
   } else {
